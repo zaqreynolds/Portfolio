552754d0aa0ebcd9436ffa9c57fc38698acd6c87
Zaq Reynolds <zaqreynolds@gmail.com>
2023-01-05 20:21:41 -0800

    Update, but not finished

---
+++ README.md
@@ -0,0 +1,5 @@
+#**Zaq Reynolds' Portfolio**
+---
+####*A constant work in progress*
+---
+v1 01.05.23
---
+++ index.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("colorflip");
+
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = "black";
+});
