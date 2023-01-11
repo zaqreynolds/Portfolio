4fa1261cc02366e17359aa89a575d204e3b00400
Zaq Reynolds <zaqreynolds@gmail.com>
2023-01-10 19:03:57 -0800

    Finish project

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-#**Zaq Reynolds' Portfolio**
+## #**Zaq Reynolds' Portfolio**
+
+####_A constant work in progress_
+
+To Do's:
+
+- make responsive for mobile devices
+- celebrate
+
 ---
-####*A constant work in progress*
----
-v1 01.05.23
+
+v1 01.10.23
--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+// const btn = document.querySelector("button");
+
+// const colorFlip = () => {
+//   document.body.style.backgroundColor = "black";
+// };
+// btn.onclick = colorFlip;
+
 const btn = document.getElementById("colorflip");
 
+let clicks = 0;
+
 btn.addEventListener("click", () => {
-  document.body.style.backgroundColor = "black";
+  clicks += 1;
+  for (let i = clicks; i > 0; i + 0) {
+    if (i % 2 != 0) {
+      document.body.style.backgroundColor = "black";
+      document.body.style.color = "snow";
+      document.querySelector(".light-theme").style.display = "none";
+      document.querySelector(".dark-theme").style.display = "block";
+    } else {
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      document.querySelector(".light-theme").style.display = "block";
+      document.querySelector(".dark-theme").style.display = "none";
+    }
+    break;
+  }
 });
